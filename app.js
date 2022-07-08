6616b9b9df3b58a25b406b1a165cbb860325d579
Arushay <piecemaster780@gmail.com>
2022-07-08 17:51:16 +0200

    1st commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// this is a comment!
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HTML 5 Boilerplate</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+	<script src="index.js"></script>
+    <p>Its a test page i just generate my ssh key for mac. pushing it to github to check connectivity.</p>
+  </body>
+</html>
